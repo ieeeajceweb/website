0feb51e91c07fcfcebea286535da8f6d2cb41e2c
Dxvan-05 <118600584+Dxvan-05@users.noreply.github.com>
2024-06-27 20:16:08 +0530

    Refactor: member-count section css

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import bootstrap from 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './navigation.css'
+import './navigation.css';
+import './member-count.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';

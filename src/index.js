88403f443022277c4e002fd25c2be6cd19afb30c
Dxvan-05 <118600584+Dxvan-05@users.noreply.github.com>
2024-06-30 04:22:47 +0530

    Feat: loader

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import bootstrap from 'bootstrap';
 
 // CSS imports
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './loader.css';
 import './navigation.css';
 import './member-count.css';
 import './logo-slide.css';
@@ -12,6 +13,7 @@ import './locomotive-scroll.css';
 
 
 // JS imports
+import './loader.js';
 import './slider.js';
 import './typed.js';
 import './smooth-scrolling.js';
--- src/smooth-scrolling.js
+++ src/smooth-scrolling.js
@@ -1,7 +1,14 @@
 import LocomotiveScroll from 'locomotive-scroll';
 
+const navigation = document.querySelector('#navigation');
+
 const scroll = new LocomotiveScroll({
     el: document.querySelector('#smooth-scrolling'),
     smooth: true,
     multiplier: 2
 });
+
+
+scroll.scrollTo(navigation);
+
+

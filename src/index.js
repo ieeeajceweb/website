afc66440bbfa8ab4855b3d95fb1b6f17b3689d14
Dxvan-05 <118600584+Dxvan-05@users.noreply.github.com>
2024-07-14 15:34:04 +0530

    Enable smooth scrolling

--- src/index.js
+++ src/index.js
@@ -9,11 +9,11 @@ import './logo-slide.css';
 import './slider.css';
 import './footer.css'
 import 'bootstrap-icons/font/bootstrap-icons.css';
-// import './locomotive-scroll.css';
+import './locomotive-scroll.css';
 
 
 // JS imports
 import './loader.js';
 import './slider.js';
 import './typed.js';
-// import './smooth-scrolling.js';
+import './smooth-scrolling.js';
